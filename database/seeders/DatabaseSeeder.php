4215be15768d353c4cd36a88a11f3f561a69d053
hara ryo <web.harada1111@gmail.com>
2023-08-11 12:11:00 +0900

    user_idのSeederを作成するコードを追記した

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-        $this->call([Tweetseeder::class]);
+        $this->call([
+            UsersSeeder::class,
+            Tweetseeder::class
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
